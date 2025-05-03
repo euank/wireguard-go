dbcea630aa23a94bfaca2b9cdb48b725ca8235fb
Euan Kemp <euank@euank.com>
2025-05-04 02:45:07 +0900

    Add initial flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "wireguard-go";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.treefmt;
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            golangci-lint
+            gofumpt
+            nixfmt-rfc-style
+          ];
+        };
+      }
+    );
+}
